63876583fc9468cb7e624fb6ea449525374fff9e
pedrvisk <pedrobrbrpedro@gmail.com>
2024-01-07 19:13:26 -0300

    Optimized and fixed welcome card

--- src/structures/extras/WelcomeCard.js
+++ src/structures/extras/WelcomeCard.js
@@ -1,6 +1,7 @@
 import { AttachmentBuilder } from 'discord.js';
 import { createCanvas, loadImage, GlobalFonts } from '@napi-rs/canvas';
 import { resolve } from 'path';
+
 GlobalFonts.registerFromPath(
   resolve('src/resources/fonts/RussoOne.ttf'),
   'RUSSOONE'
@@ -16,80 +17,67 @@ export class WelcomeCard {
       type: 'color',
       src: '#191919',
     };
-    this.title = {
-      data: 'Welcome',
-      color: '#fff',
-    };
-    this.username = {
-      data: 'Unknown',
-      color: '#fff',
-    };
-    this.description = {
-      data: '',
-      color: '#fff',
-    };
+    this.title = this.initTextConfig('Welcome', '#fff', 'RUSSOONE', 60);
+    this.username = this.initTextConfig('Unknown', '#fff', 'RUSSOONE', 40);
+    this.description = this.initTextConfig('', '#fff', 'RUSSOONE', 25);
     this.overlay = 0;
   }
 
+  initTextConfig(data, color, font, size) {
+    return { data, color, font, size };
+  }
+
   setBackground(type, value) {
-    this.background.type = type
-      ? type === 'image'
-        ? /(http(s?):)|([/|.|\w|\s])*\.(?:jpg|jpeg|png)/.test(value)
-          ? 'image'
-          : 'color'
-        : 'color'
-      : 'color';
-    if (type === null || type === 'color')
+    this.background.type =
+      type === 'image' && /\.(jpg|jpeg|png)$/.test(value) ? 'image' : 'color';
+    this.background.src =
+      (type === null || type === 'color') &&
       /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(value)
-        ? (this.background.src = value)
-        : null;
-    else if (type === 'image')
-      /(http(s?):)|([/|.|\w|\s])*\.(?:jpg|jpeg|png)/.test(value)
-        ? (this.background.src = value)
-        : null;
+        ? value
+        : this.background.src;
+    if (
+      type === 'image' &&
+      /^(http(s?):)|([/|.|\w|\s])*\.(?:jpg|jpeg|png)/.test(value)
+    ) {
+      this.background.src = value;
+    }
     return this;
   }
 
   setAvatar(value, border) {
-    /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(border)
-      ? (this.avatar.border = border)
-      : null;
-    value ? (this.avatar.src = value) : null;
-
+    this.avatar.border = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(border)
+      ? border
+      : this.avatar.border;
+    this.avatar.src = value || this.avatar.src;
     return this;
   }
 
   setTitle(value, color) {
-    /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(color)
-      ? (this.title.color = color)
-      : null;
-    value ? (this.title.data = value) : null;
-
+    this.title.color = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(color)
+      ? color
+      : this.title.color;
+    this.title.data = value || this.title.data;
     return this;
   }
 
   setUsername(value, color) {
-    /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(color)
-      ? (this.username.color = color)
-      : null;
-    value ? (this.username.data = value) : null;
-
+    this.username.color = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(color)
+      ? color
+      : this.username.color;
+    this.username.data = value || this.username.data;
     return this;
   }
-  setDescription(value, color) {
-    /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(color)
-      ? (this.description.color = color)
-      : null;
-    value ? (this.description.data = value) : null;
 
+  setDescription(value, color) {
+    this.description.color = /^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(color)
+      ? color
+      : this.description.color;
+    this.description.data = value || this.description.data;
     return this;
   }
 
   setOverlay(value) {
-    /^[0-9]*[.][0-9]+$/.test(value)
-      ? (this.overlay = value)
-      : (this.overlay = 0);
-
+    this.overlay = /^[0-9]*[.][0-9]+$/.test(value) ? value : 0;
     return this;
   }
 
@@ -97,77 +85,76 @@ export class WelcomeCard {
     const canvas = createCanvas(1024, 500);
     const ctx = canvas.getContext('2d', { alpha: false });
 
-    if (this.background.type === 'color') {
-      ctx.beginPath();
-      ctx.fillStyle = this.background.src;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-    } else if (this.background.type === 'image') {
-      try {
-        const background = await loadImage(this.background.src);
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      } catch (err) {
-        console.log(err);
-        ctx.beginPath();
-        ctx.fillStyle = '#191919';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-      }
-    }
+    await this.drawBackground(ctx, canvas);
 
-    ctx.fillStyle = `rgba(0, 0, 0, ${this.overlay})`;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    const avatarSize = 250;
-    const avatarImage = await loadImage(this.avatar.src);
-    const avatarX = (canvas.width - avatarSize) / 2;
     this.drawRoundedImageWithShadow(
       ctx,
-      avatarImage,
-      avatarX,
+      await loadImage(this.avatar.src, { alt: 'Background Image' }),
+      (canvas.width - 250) / 2,
       45,
-      avatarSize,
-      avatarSize,
-      avatarSize / 2,
+      250,
+      250,
+      125,
       this.avatar.border,
       8,
       'black',
       10
     );
 
-    ctx.font = '60px RUSSOONE';
-    ctx.fillStyle = this.title.color;
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.shadowColor = 'black';
-    ctx.shadowBlur = 4;
-    ctx.fillText(this.title.data.toUpperCase(), canvas.width / 2, 350);
-
-    ctx.font = this.applyText(canvas, ctx, 45, this.username.data);
-    ctx.fillStyle = this.username.color;
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.shadowColor = 'black';
-    ctx.shadowBlur = 4;
-    ctx.fillText(this.username.data.toUpperCase(), canvas.width / 2, 400);
-
-    if (
-      (typeof this?.description?.data === 'string' &&
-        this?.description?.data?.trim()?.length !== 0) ||
-      this?.description?.data !== null ||
-      this?.description?.data !== undefined
-    ) {
-      ctx.font = this.applyText(canvas, ctx, 35, this?.description?.data);
-      ctx.fillStyle = this.description.color;
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.shadowColor = 'black';
-      ctx.shadowBlur = 4;
-      ctx.fillText(this.description.data.toUpperCase(), canvas.width / 2, 440);
+    if (this.title.data.trim().length !== 0) {
+      this.drawText(ctx, this.title, 350, canvas);
     }
 
-    const outputBuffer = await canvas.toBuffer('image/png');
-    return new AttachmentBuilder(outputBuffer, {
-      name: 'welcome.png',
-    });
+    if (this.username.data.trim().length !== 0) {
+      this.drawText(ctx, this.username, 400, canvas);
+    }
+
+    if (this.description.data.trim().length !== 0) {
+      this.drawText(ctx, this.description, 440, canvas);
+    }
+
+    const outputBuffer = await canvas.toBuffer('image/png', { quality: 100 });
+    return new AttachmentBuilder(outputBuffer, { name: 'welcome.png' });
+  }
+
+  async drawBackground(ctx, canvas) {
+    ctx.beginPath();
+    ctx.fillStyle =
+      this.background.type === 'color' ? this.background.src : '#191919';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    if (this.background.type === 'image') {
+      try {
+        const background = await loadImage(this.background.src);
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    ctx.fillStyle = `rgba(0, 0, 0, ${this.overlay})`;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  drawText(ctx, textConfig, y, canvas) {
+    const fontSize = this.applyText(
+      ctx,
+      textConfig.size,
+      textConfig.data,
+      canvas.width
+    );
+    ctx.font = `${fontSize}px ${textConfig.font}`;
+    ctx.fillStyle = textConfig.color;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.shadowColor = 'black';
+    ctx.shadowBlur = 4;
+
+    ctx.fillStyle = textConfig.color;
+    ctx.shadowColor = 'black';
+    ctx.shadowBlur = 4;
+
+    ctx.fillText(textConfig.data.toUpperCase(), canvas.width / 2, y);
   }
 
   drawRoundedImageWithShadow(
@@ -184,8 +171,18 @@ export class WelcomeCard {
     shadowBlur
   ) {
     ctx.save();
+
+    const avatarX = x + (width - 2 * radius) / 2;
+    const avatarY = y + (height - 2 * radius) / 2;
+
     ctx.beginPath();
-    ctx.arc(x + radius, y + radius, radius + borderWidth, 0, Math.PI * 2);
+    ctx.arc(
+      avatarX + radius,
+      avatarY + radius,
+      radius + borderWidth,
+      0,
+      Math.PI * 2
+    );
     ctx.closePath();
     ctx.shadowColor = shadowColor;
     ctx.shadowBlur = shadowBlur;
@@ -195,8 +192,8 @@ export class WelcomeCard {
     this.drawRoundedImage(
       ctx,
       img,
-      x - borderWidth,
-      y - borderWidth,
+      avatarX,
+      avatarY,
       width + 2 * borderWidth,
       height + 2 * borderWidth,
       radius
@@ -205,13 +202,19 @@ export class WelcomeCard {
 
     ctx.save();
     ctx.beginPath();
-    ctx.arc(x + radius, y + radius, radius + borderWidth, 0, Math.PI * 2);
+    ctx.arc(
+      avatarX + radius,
+      avatarY + radius,
+      radius + borderWidth,
+      0,
+      Math.PI * 2
+    );
     ctx.closePath();
     ctx.fillStyle = borderColor;
     ctx.fill();
     ctx.restore();
 
-    this.drawRoundedImage(ctx, img, x, y, width, height, radius);
+    this.drawRoundedImage(ctx, img, avatarX, avatarY, width, height, radius);
   }
 
   drawRoundedImage(ctx, img, x, y, width, height, radius) {
@@ -226,13 +229,20 @@ export class WelcomeCard {
     ctx.restore();
   }
 
-  applyText = (canvas, context, initialSize, text) => {
+  applyText(context, initialSize, text, maxWidth) {
     let fontSize = initialSize;
 
-    do {
-      context.font = `${(fontSize -= 10)}px RUSSOONE`;
-    } while (context.measureText(text).width > canvas.width);
+    while (
+      this.measureTextWidth(context, `${fontSize}px RUSSOONE`, text) > maxWidth
+    ) {
+      fontSize -= 10;
+    }
 
-    return context.font;
-  };
+    return fontSize;
+  }
+
+  measureTextWidth(context, font, text) {
+    context.font = font;
+    return context.measureText(text).width;
+  }
 }
